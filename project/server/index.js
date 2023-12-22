2240357a872082942c1f74014d960023af3e73a8
narendrasamoju <narendrasamoju@gmail.com>
2023-12-22 12:33:53 +0530

    Your commit message

--- project/server/index.js
+++ project/server/index.js
@@ -3,11 +3,12 @@ import hbs from 'hbs';
 import jwt from 'jsonwebtoken';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-
-
-
+import mongoose from "mongoose"
 import { readPost, readUser, insertPost, insertUser, likeFun, shareFun, deleteFun } from './operations.js';
-
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 
 app.set('view engine','hbs')
@@ -16,20 +17,86 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"public")))
 
 
+mongoose.connect("mongodb://127.0.0.1:27017/cinema", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const screen1Model = mongoose.model('screen1', {
+    seatno: { type: Number },
+    status: { type: String }
+})
+
+const screen2Model = mongoose.model('screen2', {
+    seatno: { type: Number },
+    status: { type: String }
+})
+
+const screen3Model = mongoose.model('screen3', {
+    seatno: { type: Number },
+    status: { type: String }
+})
+
+const moviesModel = mongoose.model('movies', {
+    name: { type: String },
+    rate: { type: Number },
+    screenNo: { type: Number }
+})
+
+var screen1Res
+screen1Model.find()
+    .then((output) => {
+        screen1Res = output
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+var screen2Res
+screen2Model.find()
+    .then((output) => {
+        screen2Res = output
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+var screen3Res
+screen3Model.find()
+    .then((output) => {
+        screen3Res = output
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 
+var moviesRes
+moviesModel.find()
+    .then((output) => {
+        moviesRes = output
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
+app.get('/cinema',(req,res)=>{
+    res.render('cinema',{
+        movies:moviesRes,
+        screen1: screen1Res,
+        screen2: screen2Res,
+        screen3: screen3Res
+    })
+})
 
 
 app.get("/",(req,res)=>{
     res.render('login')
 })
 app.post("/login", async (req, res) => {
-    // console.log(req.body.profile) 
-    // console.log(req.body.password) 
-
     const output = await readUser(req.body.profile)
     const password= output[0].password
     if(password === req.body.password)
